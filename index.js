8798afaa8a8cf67fba0610be898db786a7d60c7b
thejohnjohn <jp7480566@gmail.com>
2022-11-11 22:57:17 -0300

    none

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 import { Server } from 'socket.io';
+import express from 'express';
+import { createServer } from "http";
+import { v4 as uuidv4 } from 'uuid';
 
-const io = new Server(3000, {
-  // options
+const port = 3000;
+
+const app = express();
+
+const people = { id:uuidv4(), name: 'James' }
+
+app.use(express.json());
+
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+app.get('/', (request, response) => {
+  response.send(people);
 });
 
 io.on('connection', (socket) => {
   console.log(socket.id);  
   return socket.id;
 });
+
+httpServer.listen(port);
 
