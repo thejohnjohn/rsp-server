793827220c381716ad7a12390c3de65d1aa14e91
thejohnjohn <jp7480566@gmail.com>
2022-11-13 21:19:09 -0300

    chore: add Redis

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
 import { Server } from 'socket.io';
 import express from 'express';
 import { createServer } from "http";
-import { v4 as uuidv4 } from 'uuid';
+import { createClient } from 'redis';
 
 const port = 3000;
 
 const app = express();
-
-const people = { id:uuidv4(), name: 'James' }
-
-app.use(express.json());
-
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
-app.get('/', (request, response) => {
-  response.send(people);
-});
+client.on('error', (err) => console.log('Redis Client Error', err));
+await client.connect();
 
 io.on('connection', (socket) => {
-  console.log(socket.id);  
-  return socket.id;
+  socket.on('player', (player) => {
+    const { id, gameType } = player;
+    await client.set(gameType, );
+  });
 });
 
 httpServer.listen(port);
