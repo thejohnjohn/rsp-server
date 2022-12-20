f97c8a23dcaa60176db70d9e6c412690f7150b56
thejohnjohn <jp7480566@gmail.com>
2022-12-20 09:20:10 -0300

    none

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 import { Server } from 'socket.io';
 import express from 'express';
 import { createServer } from "http";
-import { createClient } from 'redis';
+import Redis from 'ioredis';
 
 const port = 3000;
+
+const redis = new Redis(); 
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
-client.on('error', (err) => console.log('Redis Client Error', err));
-await client.connect();
+const playerList = [];
+let playerOne;
+let playerTwo;
 
 io.on('connection', (socket) => {
-  socket.on('player', (player) => {
-    const { id, gameType } = player;
-    await client.set(gameType, );
+  socket.on('player', async (player) => {
+    console.log(typeof player);
+    const roomName = '1000';
+    playerList.push(player);
+    socket.join(roomName);
+   
+    if (playerList.length >= 2) {
+      playerOne = playerList[0];
+      playerTwo = playerList[1];
+    }
+
+    if (playerTwo !== undefined) {
+      io.to(roomName).emit(playerOne.id, playerTwo);
+      io.to(roomName).emit(playerTwo.id, playerOne);
+    }
   });
 });
-
 httpServer.listen(port);
 
